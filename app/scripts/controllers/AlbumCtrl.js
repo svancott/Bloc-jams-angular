4e52984120ed19b403d0ab2edf1406a2204acbf1
svancott <thevanicotti@gmail.com>
2017-01-02 12:35:29 -0200

    assignment 2.5 song index added

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,8 @@
-(function() {
+ (function() {
      function AlbumCtrl() {
-		this.albumData = [];
-     	this.albumData.push(angular.copy(albumPicasso));
+         this.albumData = angular.copy(albumPicasso);
      }
- 
+
      angular
          .module('blocJams')
          .controller('AlbumCtrl', AlbumCtrl);
