f825f75dafd0caac42c9206883321f488ac8146b
svancott <thevanicotti@gmail.com>
2017-01-05 11:17:35 -0200

    assignment 2.9 completed

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -34,6 +34,10 @@
 					return {width: percentString()};
 				};
 				
+				scope.thumbStyle = function() {
+					return {left: percentString()};
+				};
+				
 				scope.onClickSeekBar = function(event) {
 					var percent = calculatePercent(seekBar, event);
 					scope.value = percent * scope.max;
